bc31a0ce3aa531710104096d2aac5a969b576230
skysalisbury <skysalisbury@gmail.com>
2025-06-05 17:44:46 -0400

    Work on comments

--- backend/controllers/hoots.js
+++ backend/controllers/hoots.js
@@ -1,5 +1,4 @@
-const Hoot = require('../models/hoot');
-
+const Hoot = require("../models/hoot");
 
 module.exports = {
   index,
@@ -12,44 +11,45 @@ module.exports = {
 async function index(req, res) {
   try {
     const hoots = await Hoot.find({});
-    // Below would return all posts for just the logged in user
-    // const posts = await Post.find({author: req.user._id});
+    // Below would return all hoots for just the logged in user
+    // const hoots = await Hoot.find({author: req.user._id});
     res.json(hoots);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Failed to fetch posts' });
+    res.status(500).json({ message: "Failed to fetch hoots" });
   }
 }
-
 async function create(req, res) {
   try {
     req.body.author = req.user._id;
     const hoot = await Hoot.create(req.body);
+    // Below would return all hoots for just the logged in user
+    // const hoots = await Hoot.find({author: req.user._id});
     res.json(hoot);
   } catch (err) {
     console.log(err);
-    res.status(400).json({ message: 'Failed to fetch posts' });
+    res.status(400).json({ message: "Failed to create hoot" });
   }
 }
 
 async function show(req, res) {
   try {
-    const hoot = await Hoot.findById(req.params.hootId).populate('author');
+    const hoot = await Hoot.findById(req.params.hootId).populate("author");
     res.json(hoot);
   } catch (err) {
     console.log(err);
-    res.status(400).json({ message: 'Failed to fetch posts' });
+    res.status(400).json({ message: "Failed to fetch posts" });
   }
 }
 
 async function update(req, res) {
   try {
-   const hoot = await Hoot.findById(req.params.hootId);
+    const hoot = await Hoot.findById(req.params.hootId);
     // Check permissions:
     if (!hoot.author.equals(req.user._id)) {
       return res.status(403).send("You're not allowed to do that!");
     }
-      const updatedHoot = await Hoot.findByIdAndUpdate(
+    const updatedHoot = await Hoot.findByIdAndUpdate(
       req.params.hootId,
       req.body,
       { new: true }
@@ -59,12 +59,12 @@ async function update(req, res) {
     res.json(updatedHoot);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Failed to update post' });
+    res.status(500).json({ message: "Failed to update post" });
   }
 }
 
 async function deleteHoot(req, res) {
-   try {
+  try {
     const hoot = await Hoot.findById(req.params.hootId);
 
     if (!hoot.author.equals(req.user._id)) {
@@ -76,4 +76,4 @@ async function deleteHoot(req, res) {
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
-};
+}
